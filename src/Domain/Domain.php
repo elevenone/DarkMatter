aff187328508c0ddd3f5983bdbf871a6f7c9c450
mikka <ezsolt@gmail.com>
2020-10-23 20:20:48 +0200

    Removing payload

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace DarkMatter\Domain;
 
 use DarkMatter\Components\Logger\LoggerInterface;
-use DarkMatter\Payload\Payload;
-use DarkMatter\Payload\Status;
 
 abstract class Domain
 {
